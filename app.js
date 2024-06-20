5a582f6f313fb0836822db6365b3eff4b47eebd3
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-06-20 17:55:44 +0200

    added: sockets in a list

--- app.js
+++ app.js
@@ -10,15 +10,33 @@ app.get('/', function(req, res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
-
 console.info('server started');
+
+const SOCKET_LIST = {};
 
 // Socket.io section (Client/Server communication)
 const io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
     console.log('socket connection');
+    
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    SOCKET_LIST['socket.id'] = socket;
 
-    socket.on('happyMsg', () => {
-        console.log('I am happy !!!!!');
-    })
+    // socket.on('client2server', (data) => {
+    //     console.log('client2server message + ' + data.reason);
+    // })
 });
+
+setInterval(() => {
+    for (let i in SOCKET_LIST) {
+        const socket = SOCKET_LIST[i];
+        socket.x++;
+        socket.y++;
+        socket.emit('newPosition' {
+            x: socket.x,
+            y: socket.y
+        })
+    }
+}, 1000/25);
